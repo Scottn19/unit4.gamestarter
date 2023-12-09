2215436152ef0b0de08c6ba58ddb6063196d1dc7
Scottn19 <141892117+Scottn19@users.noreply.github.com>
2023-12-09 09:58:35 -0700

    Updated videoGames.js

--- Unit4.Gamestore.Starter/api/videoGames.js
+++ Unit4.Gamestore.Starter/api/videoGames.js
@@ -17,12 +17,16 @@ router.get('/', async (req, res, next) => {
     } catch (error) {
         next(error);
     }
+// send in a way to see our post
+   console.log(req.body);
+   res.send("will post later")
 });
 
 // GET - /api/video-games/:id - get a single video game by id
 router.get('/:id', async (req, res, next) => {
     try {
-        const videoGame = await getVideoGameById(REPLACE_ME);
+        const id = req.params.id;
+        const videoGame = await getVideoGameById(id);
         res.send(videoGame);
     } catch (error) {
         next(error);
@@ -30,19 +34,38 @@ router.get('/:id', async (req, res, next) => {
 });
 
 // POST - /api/video-games - create a new video game
-router.patch('/', async (req, res, next) => {
+router.post('/', async (req, res, next) => {
     // LOGIC GOES HERE 
+    try {
+        const videoGame = await createVideoGame(req.body);
+        res.send(videoGame);
+    } catch (error) {
+        next(error);
+    }
 });
 
 
 // PUT - /api/video-games/:id - update a single video game by id
 router.put('/:id', async (req, res, next) => {
     // LOGIC GOES HERE 
+    try {
+        const videoGame = await updateVideoGame(req.params.id, req.body);
+        res.send(videoGame);
+    } catch (error) {
+        next(error);
+    }
 });
 
 // DELETE - /api/video-games/:id - delete a single video game by id
 router.delete('/:id', async (req, res, next) => {
     // LOGIC GOES HERE
+    try {
+        const videoGame = await deleteVideoGame(req.params.id);
+        res.send(videoGame);
+    } catch (error) {
+        next(error);
+    
+    }
 });
 
 module.exports = router;
